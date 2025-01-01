33b37149008fa0308a168d6466b59fec1d29a31e
Raghuram Subramani <raghus2247@gmail.com>
2025-01-01 08:04:09 -0500

    all: Copyright: 2024-2025

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 
 #
 # CMOS
-# Copyright (C) 2024 Raghuram Subramani <raghus2247@gmail.com>
+# Copyright (C) 2024-2025 Raghuram Subramani <raghus2247@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
