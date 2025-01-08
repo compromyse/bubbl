b82649c2b156d5351e15fbbde00d09e5a8917b26
Raghuram Subramani <raghus2247@gmail.com>
2025-01-08 13:46:53 +0530

    build: cmake: Restructure

---
+++ cmake/run.cmake
@@ -0,0 +1,53 @@
+set(BOCHS_CMD bochs)
+set(QEMU_SYSTEM_CMD qemu-system-i386)
+
+set(QEMU_ARGUMENTS
+  -cdrom ${CMAKE_BINARY_DIR}/bubbl.iso
+  -device isa-debug-exit,iobase=0xf4,iosize=0x04
+  -m 32M
+  -smp 1
+)
+
+set(IGNORE_EXIT || true)
+
+add_custom_target(run
+  ${QEMU_SYSTEM_CMD}
+  ${QEMU_ARGUMENTS}
+  -serial stdio
+
+  ${IGNORE_EXIT}
+
+  DEPENDS iso
+  USES_TERMINAL
+)
+
+add_custom_target(run-headless
+  ${QEMU_SYSTEM_CMD}
+  ${QEMU_ARGUMENTS}
+  -nographic
+
+  ${IGNORE_EXIT}
+
+  DEPENDS iso
+  USES_TERMINAL
+)
+
+add_custom_target(run-bochs
+  ${BOCHS_CMD}
+  -f ${CMAKE_SOURCE_DIR}/.bochsrc
+
+  DEPENDS iso
+  USES_TERMINAL
+)
+
+add_custom_target(run-gdb
+  ${QEMU_SYSTEM_CMD}
+  ${QEMU_ARGUMENTS}
+  -s
+  -S
+
+  ${IGNORE_EXIT}
+
+  DEPENDS iso
+  USES_TERMINAL
+)
